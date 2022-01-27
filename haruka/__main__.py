a50248faddbcb279d9c5c16c8b5062d5546933b2
Jayanti Chaudhary <82433052+jayanti01@users.noreply.github.com>
2022-01-27 07:52:15 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -20,7 +20,7 @@ from haruka.modules.helper_funcs.misc import paginate_modules
 from haruka.modules.translations.strings import tld, tld_help 
 from haruka.modules.connection import connected
 
-PM_START = """Hello {}, my name is {}!
+PM_START = """Hello , my name is Nonsense Robot!
 
 You know how hard it is sometimes to manage group so here is the solution for you
 
@@ -29,17 +29,17 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [Jay 🇮🇳](t.me/chaudhary_Anjana)
 
 Special thanks to [this person](t.me/denver02) for helping me.
 
-Deploy a bot like me [now](www.github.com/xditya/GroupManager)
+Deploy a bot like me [now](www.github.com/jayanti01/GroupManager)
 
 Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu.be) on deploying me.
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/Nonsense_proRobot?startgroup=true).
 """
 
 
